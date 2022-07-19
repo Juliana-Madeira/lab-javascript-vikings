7225c4490aefdf28dacf70e959d4d12bcce95758
Juliana Madeira <79094884+Juliana-Madeira@users.noreply.github.com>
2022-07-19 16:40:35 -0300

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -55,27 +55,51 @@ class Saxon extends Soldier {
 
 // War
 class War {
-  constructor(){
-    this.vikingArmy = [];
-    this.saxonArmy = [];
-  };
-  addViking(viking){
-    this.vikingArmy.push(viking);
-  };
-  addSaxon(saxon){
-    this.saxonArmy.push(saxon);
-  };
-  vikingAttack(){
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        let randomViking = this.vikingArmy[randomVikingIndex]   //qual viking que é 
+        let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length)   //sorteio um index de um saxon
+        let randomSaxon = this.saxonArmy[randomSaxonIndex]      //qual saxon é
+        //acima foram escolhidos aleatoriamente o viking e o saxon que estao lutando
 
-  };
-  saxonAttack(){
+        let fight = randomSaxon.receiveDamage(randomViking.attack())
+        if(randomSaxon.health <= 0){
+            this.saxonArmy.splice(randomSaxonIndex, 1)
+        }
+        return fight
+    }
+    saxonAttack(){
+        let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        let randomViking = this.vikingArmy[randomVikingIndex]   //qual viking que é 
+        let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length)   //sorteio um index de um saxon
+        let randomSaxon = this.saxonArmy[randomSaxonIndex]      //qual saxon é
+        //acima foram escolhidos aleatoriamente o viking e o saxon que estao lutando
 
-  };
-  showStatus(){
-    
-  } 
-}
-
+        let fight = randomViking.receiveDamage(randomSaxon.attack())
+        if(randomViking.health <= 0){
+            this.vikingArmy.splice(randomVikingIndex, 1)
+        }
+        return fight
+    }
+    showStatus(){
+        if(this.vikingArmy.length > 0 && this.saxonArmy.length > 0){
+            return `Vikings and Saxons are still in the thick of battle.`
+        } else if(this.vikingArmy.length === 0){
+            return `Saxons have fought for their lives and survived another day...`
+        } else {
+            return `Vikings have won the war of the century!`
+        }
+    }
 
 
 // The following is required to make unit tests work.
